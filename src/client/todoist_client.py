2bce911faa4c9c2ef87c86ac5bb0c6fab12bcb4f
userS4B0 <users4b0@proton.me>
2025-11-24 18:28:04 +0100

    Implemented basic todoist api client functions

--- src/client/todoist_client.py
+++ src/client/todoist_client.py
@@ -1,5 +1,5 @@
 from todoist_api_python.api import TodoistAPI
-from config.settings import TODOIST_API_TOKEN, NONSCHEDULED_TASKS_LABEL
+from config.settings import TODOIST_API_TOKEN, DEFAULT_TABLEFORMAT
 from tabulate import tabulate
 from utils.time_utils import normalize_datetime
 
@@ -8,45 +8,28 @@ class TodoistClient(TodoistAPI):
     def __init__(self):
         super().__init__(TODOIST_API_TOKEN)
 
-    def get_nonscheduled_tasks(self):
-        print(
-            f"[DEBUG] Obteniendo lista de tareas con label {NONSCHEDULED_TASKS_LABEL}"
-        )
-
-        try:
-            return self.get_tasks(label=NONSCHEDULED_TASKS_LABEL)
-        except Exception as e:
-            raise RuntimeError(f"Error in Todoist communication: {e}")
-
-    def show_task_table(tasks: list):
+    @staticmethod
+    def show_task_table(tasks):
+        table_data = []
 
         for task in tasks:
             formatted_due = (
                 normalize_datetime(task.due.datetime)
                 if task.due and task.due.datetime
-                else None
+                else "—"
             )
-        table_data = [
-            [
-                task.id,
-                task.content,
-                formatted_due if (task.due and task.due.date) else "—",
-                "Yes" if task.is_completed else "No",
-            ]
-        ]
 
-        headers = ["ID", "Task", "Due Date", "Completed"]
-        return tabulate(table_data, headers=headers, tablefmt="github")
-
-    def show_task_list(tasks: list):
-        print("[DEBUG] Mostrando tareas en formato lista")
-
-        for task in tasks:
-            formatted_due = (
-                normalize_datetime(task.due.datetime)
-                if task.due and task.due.datetime
-                else None
+            table_data.append(
+                [
+                    task.priority,
+                    task.content,
+                    task.description,
+                    task.labels,
+                    formatted_due,
+                    "Yes" if task.is_completed else "No",
+                ]
             )
-            print(f"[P{task.priority}] {task.content}")
-            print(f"  ID:  {task.id}")
-            print(f"  Due: {formatted_due}\n")
+
+        headers = ["Priority", "ID", "Task", "Due Date", "Completed"]
+        print(tabulate(table_data, headers=headers, tablefmt=DEFAULT_TABLEFORMAT))
+
--- src/main.py
+++ src/main.py
@@ -1,15 +1,13 @@
 from client.todoist_client import TodoistClient
+from config.settings import NONSCHEDULED_TASKS_LABEL
+
 
 def main():
     todoist_client = TodoistClient()
 
     print("\n== TAREAS SIN AGENDAR ==")
-    tasks = todoist_client.get_nonscheduled_tasks()
-    print("[DEBUG] Lista de tareas con label Obtenida")
-    todoist_client.show_task_list(tasks)
-    print("[DEBUG] Tareas en formato lista mostradas")
-
-
+    tasks = todoist_client.get_tasks(label=NONSCHEDULED_TASKS_LABEL)
+    todoist_client.show_task_table(tasks)
 
 if __name__ == "__main__":
     main()
