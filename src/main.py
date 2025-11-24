055e90bb2fa0ac0267dc4c3cef33702412c6b139
userS4B0 <users4b0@proton.me>
2025-11-24 18:40:12 +0100

    Implemented lean output for keyboard interrupt events

--- src/main.py
+++ src/main.py
@@ -5,9 +5,12 @@ from config.settings import NONSCHEDULED_TASKS_LABEL
 def main():
     todoist_client = TodoistClient()
 
-    print("\n== TAREAS SIN AGENDAR ==")
+    print(f"\n[INFO] Fetching tasks for label: {NONSCHEDULED_TASKS_LABEL}\n")
     tasks = todoist_client.get_tasks(label=NONSCHEDULED_TASKS_LABEL)
     todoist_client.show_task_table(tasks)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n[INFO] Program interrupted by user. Exiting cleanly...\n")
