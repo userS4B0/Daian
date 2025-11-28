73f6c3f6e9110b882f5b459bdae80f4c5217831c
userS4B0 <74874039+userS4B0@users.noreply.github.com>
2025-11-29 00:19:11 +0100

    Basic Log implementation (#3)

--- src/config/user_settings.py
+++ src/config/user_settings.py
@@ -1,16 +1,30 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+from config.log.logger import setup_logger
 
-# ----------------------------------------------------------------------
-# Google calendar IDs (defined on .env)
-# ----------------------------------------------------------------------
-GCAL_ID_TASKS=os.getenv("GCAL_ID_TASKS")
-GCAL_ID_WORK=os.getenv("GCAL_ID_WORK")
-GCAL_ID_TIMEMANAGE=os.getenv("GCAL_ID_TIMEMANAGE")
-GCAL_ID_PERSONALEVENTS=os.getenv("GCAL_ID_PERSONALEVENTS")
+logger = setup_logger(__name__)
+
+# Load environment variables from .env file
+logger.debug("Loading user settings")
+try:
+    load_dotenv()
+
+    # ----------------------------------------------------------------------
+    # Google calendar IDs (defined on .env)
+    # ----------------------------------------------------------------------
+    GCAL_ID_TASKS = os.getenv("GCAL_ID_TASKS")
+    GCAL_ID_WORK = os.getenv("GCAL_ID_WORK")
+    GCAL_ID_TIMEMANAGE = os.getenv("GCAL_ID_TIMEMANAGE")
+    GCAL_ID_PERSONALEVENTS = os.getenv("GCAL_ID_PERSONALEVENTS")
+
+    # ----------------------------------------------------------------------
+    # Default user timezone (defined on .env)
+    # ----------------------------------------------------------------------
+    DEF_TZ = os.getenv("DEF_TZ")
+
+except Exception as e:
+    logger.error(f"Error loading user settings: {e}")
 
 # ----------------------------------------------------------------------
 # Default label for non-scheduled tasks
@@ -18,7 +32,6 @@ GCAL_ID_PERSONALEVENTS=os.getenv("GCAL_ID_PERSONALEVENTS")
 NONSCHEDULED_TASKS_LABEL = "Agendar"
 
 # ----------------------------------------------------------------------
-# Default user timezone (defined on .env)
+# Fallback TimeZone
 # ----------------------------------------------------------------------
-DEF_TZ = os.getenv("DEF_TZ")
 FALLBACK_TZ = "UTC"
