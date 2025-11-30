ffd86172f6f0bce9358fc5355edebf841a0281ac
userS4B0 <users4b0@proton.me>
2025-11-30 18:58:53 +0100

    Labeling todos

--- src/client/todoist_client.py
+++ src/client/todoist_client.py
@@ -18,7 +18,7 @@ class TodoistClient(TodoistAPI):
     # BUG: Handle connection failed errors
     # Handle program errors when connection to todoist API fails
     # assignees: userS4B0
-    # labels: priority_medium, bug, todoist_api
+    # labels: priority_medium, todoist, model_side
     # milestone: v1.0.0
 
     # ----- Main Constructor -----------------------------------------------
