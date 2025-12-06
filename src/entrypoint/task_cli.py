6e2343cd683ebf662e056aa32e810422e68a112e
userS4B0 <users4b0@proton.me>
2025-12-06 13:38:20 +0100

    cli:integrated append-nonshcedule cli function

--- src/entrypoint/task_cli.py
+++ src/entrypoint/task_cli.py
@@ -5,6 +5,7 @@ from client.todoist_client import TodoistClient
 
 tasks_app = typer.Typer(help="Task-related commands")
 
+
 # ----- List tasks -----------------------------------------------------
 @tasks_app.command("list-all", help="Lists all tasks & filter quantity by limit.")
 def list_all(limit: int = typer.Option(10, help="Limit number of tasks")):
@@ -12,25 +13,31 @@ def list_all(limit: int = typer.Option(10, help="Limit number of tasks")):
     todoist_client = TodoistClient()
     all_tasks = todoist_client.get_all_tasks(limit)
 
-    print(f"[bold cyan]Showing first {limit} task(s)[/bold cyan]\n{todoist_client.tasks_totable(all_tasks)}")
+    print(
+        f"[bold cyan]Showing first {limit} task(s)[/bold cyan]\n{todoist_client.tasks_totable(all_tasks)}"
+    )
 
 
-@tasks_app.command("list-expired",help="List tasks with expired due date by 24h.")
+@tasks_app.command("list-expired", help="List tasks with expired due date by 24h.")
 def list_expired():
     """Show all expired tasks."""
     todoist_client = TodoistClient()
     expired_tasks = todoist_client.get_expired_tasks()
 
-    print(f"[bold red]Expired tasks:[/bold red]\n{todoist_client.tasks_totable(expired_tasks)}")
+    print(
+        f"[bold red]Expired tasks:[/bold red]\n{todoist_client.tasks_totable(expired_tasks)}"
+    )
 
 
-@tasks_app.command("list-nonscheduled",help="List tasks ready to re-schedule.")
+@tasks_app.command("list-nonscheduled", help="List tasks ready to re-schedule.")
 def list_nonscheduled():
     """Show tasks with no due date assigned."""
     todoist_client = TodoistClient()
     nonscheduled_tasks = todoist_client.get_nonscheduled_tasks()
 
-    print(f"[bold yellow]Tasks without schedule:[/bold yellow]\n{todoist_client.tasks_totable(nonscheduled_tasks)}")
+    print(
+        f"[bold yellow]Tasks without schedule:[/bold yellow]\n{todoist_client.tasks_totable(nonscheduled_tasks)}"
+    )
 
 
 @tasks_app.command("estimate", help="Estimates task completion time.")
@@ -43,4 +50,28 @@ def estimate_task(task_id: str):
     estimator = HeuristicEstimator()
     estimation_result = estimator.estimate(task)
 
-    print(f"\n[bold green]Estimation result:[/bold green]\n{estimator.estimation_totable(estimation_result)}")
+    print(
+        f"\n[bold green]Estimation result:[/bold green]\n{estimator.estimation_totable(estimation_result)}"
+    )
+
+
+@tasks_app.command(
+    "append-nonschedule",
+    help="Appends overdue tasks to nonscheduled query for re-scheduling",
+)
+def append_nonschedule():
+    from core.planner import Planner
+
+    todoist_client = TodoistClient()
+
+    expired_tasks = todoist_client.get_expired_tasks()
+
+    print(
+        f"[bold red]Expired tasks:[/bold red]\n{todoist_client.tasks_totable(expired_tasks)}"
+    )
+
+    planner = Planner(todoist_client)
+
+    planner.add_to_nonscheduled_queue(expired_tasks)
+
+    print("[bold green]New tasks added to re-schedule queue![/bold green]\n")
