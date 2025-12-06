1f8759241a9c5b12e62e7705c3a31ef4d577b2bb
userS4B0 <users4b0@proton.me>
2025-12-06 12:53:12 +0100

    cli: defined basic task listing functionalities

--- src/entrypoint/task_cli.py
+++ src/entrypoint/task_cli.py
@@ -1,26 +1,45 @@
 import typer
 from rich import print
 
+from client.todoist_client import TodoistClient
+
 tasks_app = typer.Typer(help="Task-related commands")
 
-@tasks_app.command("list-expired")
-def list_tasks():
-    from client.todoist_client import TodoistClient
+# ----- List tasks -----------------------------------------------------
+@tasks_app.command("list-all", help="Lists all tasks & filter quantity by limit.")
+def list_all(limit: int = typer.Option(10, help="Limit number of tasks")):
+    """List all tasks."""
+    todoist_client = TodoistClient()
+    all_tasks = todoist_client.get_all_tasks(limit)
 
-    client = TodoistClient()
-    tasks = client.get_expired_tasks()
+    print(f"[bold cyan]Showing first {limit} task(s)[/bold cyan]\n{todoist_client.tasks_totable(all_tasks)}")
 
 
-    print(f"[bold cyan]Showing expired tasks[/bold cyan]\n{client.tasks_totable(tasks)}")
-    
-  
-@tasks_app.command("estimate")
+@tasks_app.command("list-expired",help="List tasks with expired due date by 24h.")
+def list_expired():
+    """Show all expired tasks."""
+    todoist_client = TodoistClient()
+    expired_tasks = todoist_client.get_expired_tasks()
+
+    print(f"[bold red]Expired tasks:[/bold red]\n{todoist_client.tasks_totable(expired_tasks)}")
+
+
+@tasks_app.command("list-nonscheduled",help="List tasks ready to re-schedule.")
+def list_nonscheduled():
+    """Show tasks with no due date assigned."""
+    todoist_client = TodoistClient()
+    nonscheduled_tasks = todoist_client.get_nonscheduled_tasks()
+
+    print(f"[bold yellow]Tasks without schedule:[/bold yellow]\n{todoist_client.tasks_totable(nonscheduled_tasks)}")
+
+
+@tasks_app.command("estimate", help="Estimates task completion time.")
 def estimate_task(task_id: str):
     from client.todoist_client import TodoistClient
     from core.heuristic_estimator import HeuristicEstimator
 
-    client = TodoistClient()
-    task = client.get_task(task_id)
+    todoist_client = TodoistClient()
+    task = todoist_client.get_task(task_id)
 
     estimator = HeuristicEstimator()
     result = estimator.estimate(task)
