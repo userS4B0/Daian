bccf1331f8252d294309d7ebb75860dd58d2b0f4
userS4B0 <users4b0@proton.me>
2025-12-07 14:21:13 +0100

    core: change deprecated log file name on logger class

--- src/config/log/logger.py
+++ src/config/log/logger.py
@@ -3,13 +3,11 @@ import logging.handlers
 
 from pathlib import Path
 
-# ----------------------------------------------------------------------
-# Logger Variables
-# ----------------------------------------------------------------------
+# ----- Logger Variables -----------------------------------------------
 LOG_DIR = Path("logs")
 LOG_DIR.mkdir(exist_ok=True)
 
-LOG_FILE = LOG_DIR / "mydailyplanner.log"
+LOG_FILE = LOG_DIR / "daian.log"
 
 
 def setup_logger(logger_name: str) -> object:
@@ -34,7 +32,8 @@ def setup_logger(logger_name: str) -> object:
 
     # Optional Debug console
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.WARNING)  # no ensucia tu CLI
+    console_handler.setLevel(logging.DEBUG)
+    # console_handler.setLevel(logging.WARNING)
     console_handler.setFormatter(formatter)
 
     # Register Handlers
